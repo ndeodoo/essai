f5caa6bc2af68715c15ad5d7be17ccf1c9cd9ee6
Nathan de Pryck <nde@odoo.com>
2019-09-10 16:00:26 +0200

    Commit branch 10.0

---
+++ addon1/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Module1',
+    'category': 'Tools',
+    'summary': '_module1',
+    'description': """
+This module gives a framework for SMS text messaging
+----------------------------------------------------
+The service is provided by the In App Purchase Odoo platform.
+""",
+    'depends': ['addon2','addon3'],
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    #'price': 115,
+    'currency': 'EUR',
+    'license': 'LGPL-3',
+    'support': 'test@test.com',
+}
---
+++ addon2/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Module2',
+    'category': 'Tools',
+    'summary': '_module2',
+    'description': """
+This module gives a framework for SMS text messaging
+----------------------------------------------------
+The service is provided by the In App Purchase Odoo platform.
+""",
+    'depends': ['addon4'],
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'price': 115,
+    'currency': 'EUR',
+    'license': 'LGPL-3',
+    'support': 'test@test.com',
+}
