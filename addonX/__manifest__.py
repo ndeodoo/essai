6a79e469708459563482e84ad417ccbeeed1980b
Nathan de Pryck <nde@odoo.com>
2019-12-04 15:27:28 +0100

    test repo

--- addonX/__manifest__.py
+++ addonX/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Module1',
+    'name': 'ModuleZ',
     'category': 'Tools',
-    'summary': '_module1',
+    'summary': '_moduleZ',
     'description': """
 This module gives a framework for SMS text messaging
 ----------------------------------------------------
 The service is provided by the In App Purchase Odoo platform.
 """,
-    'depends': ['addon4','addon5','addonZ','not_existing_module'],
+    'depends': ['redsfsrf' ],
     'data': [
     ],
     'qweb': [
     ],
     'installable': True,
     'auto_install': True,
-    #'price': 115,
+    'price': 500,
     'currency': 'EUR',
     'license': 'LGPL-3',
     'support': 'test@test.com',
